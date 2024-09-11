3da2fb51030feb9d385a7451ec895acc11387fe1
Fan Shu <fhu9@gatech.edu>
2024-09-11 00:04:57 -0500

    minamal example

---
+++ reasoning/CoT.py
@@ -0,0 +1,9 @@
+import dspy
+
+class CoT(dspy.Module):
+    def __init__(self):
+        super().__init__()
+        self.prog = dspy.ChainOfThought("question -> answer")
+
+    def forward(self, question):
+        return self.prog(question=question)
